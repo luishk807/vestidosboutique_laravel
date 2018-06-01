a4756d28f5696c6ec86c1a756f5147fc90b2d4a5
luishk807 <evil_luis@hotmail.com>
2018-06-01 17:23:58 -0400

    rmoved and created new sass files

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,48 @@
+<div class="container">
+    <div class="row vesti-footer-section-1">
+        <div class="col-md-4  text-center">
+            Vestidos Boutique
+        </div>
+
+        <div class="col-md-8  text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center">
+                        <ul class="list-unstyled">
+                            <li>About Us</li>
+                            <li>Contact Us</li>
+                            <li>Faqs</li>
+                            <li>Terms of Use</li>
+                            <li>Privacy</li>
+                        </ul>
+
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <ul class="list-unstyled">
+                            <li>Got Questions? Call Us</li>
+                            <li>PHONE: +507 203-5848</li>
+                            <li>EMAIL: info@vestidosboutique.com</li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row vesti-footer-section-2">
+        <div class="col-md-4 text-center">
+            We Accept:
+            <img src="{{ asset('images/cc-visa.svg') }}" class="vesti-svg vestidos-icons-payment"/>
+            <img src="{{ asset('images/cc-master.svg') }}" class="vesti-svg vestidos-icons-payment"/>
+            <img src="{{ asset('images/cc-amex.svg') }}" class="vesti-svg vestidos-icons-payment"/>
+            <img src="{{ asset('images/cc-discover.svg') }}" class="vesti-svg vestidos-icons-payment"/>
+        </div>
+
+        <div class="col-md-4 text-center">
+            <img src="{{ asset('images/social-facebook.svg') }}" class="vesti-svg vestidos-icons-social-b"/>
+            <img src="{{ asset('images/social-instagram.svg') }}" class="vesti-svg vestidos-icons-social"/>
+            <img src="{{ asset('images/social-twitter.svg') }}" class="vesti-svg vestidos-icons-social"/>
+            <img src="{{ asset('images/social-pinterest.svg') }}" class="vesti-svg vestidos-icons-social"/>
+        </div>
+    </div>
+</div>
