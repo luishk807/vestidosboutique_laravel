6149dff16505d0a4aceb56bb210160c0183569a8
luishk807 <evil_luis@hotmail.com>
2018-06-02 18:20:56 -0400

    more mixing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,225 +1,6 @@
 @extends("layouts.app")
 @section('content')
     <style>
-        
-       @media only screen and (max-device-width: 812px) and (orientation: landscape) {
-           .vestidos-footer{
-            padding-top: 50px;
-           }
-           .vestidos-main-nav-top{
-            margin-top: -10px;
-           }
-           #vesti-main-nav-btn{
-            height: 26px;
-           }
-           #vesti-main-nav-btn span:nth-child(2),
-           #vesti-main-nav-btn span:nth-child(3){
-               top:10px;
-           }
-           #vesti-main-nav-btn span:nth-child(4){
-            top: 21px;
-           }
-           .vestidos-main-nav{
-            height: 40px;
-           }
-           #main_slider_arrow_cont{
-               display:none;
-           }
-           .main_slider_txt{
-            padding-top: 20px;
-           }
-           .fp-controlArrow.fp-prev, .fp-controlArrow.fp-next{
-               display:none;
-           }
-
-           #home_main_slider .fp-tableCell{
-            vertical-align: middle;
-           }
-           #home_main_slider .main_slider_btn{
-            text-align: center;
-           }
-           #home_main_slider .container .main_slider_txt span:nth-child(1),
-           #home_main_slider .container .main_slider_txt span:nth-child(3){
-            font-size: 2rem !important;
-           }
-           home_main_slider .container .main_slider_txt .main_slider_in{
-            padding-right: 33px;
-           }
-           #slide1, #slide2, #slide3{
-            background-position: top center;
-           }
-           .btn-vesti-slide{
-            padding: 5px 53px;
-            font-size: 1rem;
-           }
-           #top_middle_sec #top_middle_sec_row img{
-               width:220px;
-           }
-           #top_middle_sec #top_middle_sec_row{
-            height: 381px;
-           }
-            #top_middle_sec_row #top_middle_img1{
-            left:0px;
-            }
-            #top_middle_sec_row #top_middle_img2{
-            left:240px;
-            }
-            #top_middle_sec_row #top_middle_img3{
-            left:478px;
-            }
-            #top_middle_sec_row .vesti-heart-link{
-                right: 8px;
-            }
-            #top_middle_sec_row .vesti-new-txt,
-            #quince_selec_sec .vesti-new-txt{
-                font-size: .8rem;
-                top: 12px;
-                left: 24px;
-            }
-            #top_middle_sec_row .vesti-new-border,
-            #quince_selec_sec .vesti-new-border{
-                border-top: 70px solid #87124a;
-                border-right: 70px solid transparent;
-            }
-            #fp-nav{
-                display:none
-            }
-            #top_middle_sec .top_middle_sec_title2{
-                font-size: 1.6rem;
-            }
-            .brands_txt > div,
-            .quince_txt > div{
-                max-width: 412px;
-            }
-            #brands_section .brands_txt div:first-child,
-            #quince_main .quince_txt div:first-child{
-                font-size: 2rem;
-                text-align: left;
-            }
-            #quince_main,
-            #brands_section{
-                padding-bottom:20px !important;
-            }
-            #brands_section .brands_txt div:last-child,
-            #quince_main .quince_txt div:last-child{
-                font-size: 1rem;
-            }
-            #quince_selec_sec .quince-select-title{
-                font-size: 2rem;
-            }
-        }
-         @media only screen and (max-device-width: 812px) and (orientation: landscape) {
-           .vestidos-footer{
-            padding-top: 50px;
-           }
-           .vestidos-main-nav-top{
-            margin-top: -10px;
-           }
-           #vesti-main-nav-btn{
-            height: 26px;
-           }
-           #vesti-main-nav-btn span:nth-child(2),
-           #vesti-main-nav-btn span:nth-child(3){
-               top:10px;
-           }
-           #vesti-main-nav-btn span:nth-child(4){
-            top: 21px;
-           }
-           .vestidos-main-nav{
-            height: 40px;
-           }
-           #main_slider_arrow_cont{
-               display:none;
-           }
-           .main_slider_txt{
-            padding-top: 20px;
-           }
-           .fp-controlArrow.fp-prev, .fp-controlArrow.fp-next{
-               display:none;
-           }
-
-           #home_main_slider .fp-tableCell{
-            vertical-align: middle;
-           }
-           #home_main_slider .main_slider_btn{
-            text-align: center;
-           }
-           #home_main_slider .container .main_slider_txt span:nth-child(1),
-           #home_main_slider .container .main_slider_txt span:nth-child(3){
-            font-size: 2rem !important;
-           }
-           home_main_slider .container .main_slider_txt .main_slider_in{
-            padding-right: 33px;
-           }
-           #slide1, #slide2, #slide3{
-            background-position: top center;
-           }
-           .btn-vesti-slide{
-            padding: 5px 53px;
-            font-size: 1rem;
-           }
-           #top_middle_sec #top_middle_sec_row img{
-               width:220px;
-           }
-           #top_middle_sec #top_middle_sec_row{
-            height: 381px;
-           }
-            #top_middle_sec_row #top_middle_img1{
-            left:0px;
-            }
-            #top_middle_sec_row #top_middle_img2{
-            left:240px;
-            }
-            #top_middle_sec_row #top_middle_img3{
-            left:478px;
-            }
-            #top_middle_sec_row .vesti-heart-link{
-                right: 8px;
-            }
-            #top_middle_sec_row .vesti-new-txt,
-            #quince_selec_sec .vesti-new-txt{
-                font-size: .8rem;
-                top: 12px;
-                left: 24px;
-            }
-            #top_middle_sec_row .vesti-new-border,
-            #quince_selec_sec .vesti-new-border{
-                border-top: 70px solid #87124a;
-                border-right: 70px solid transparent;
-            }
-            #fp-nav{
-                display:none
-            }
-            #top_middle_sec .top_middle_sec_title2{
-                font-size: 1.6rem;
-            }
-            .brands_txt > div,
-            .quince_txt > div{
-                max-width: 412px;
-            }
-            #brands_section .brands_txt div:first-child,
-            #quince_main .quince_txt div:first-child{
-                font-size: 2rem;
-                text-align: left;
-            }
-            #quince_main,
-            #brands_section{
-                padding-bottom:20px !important;
-            }
-            #brands_section .brands_txt div:last-child,
-            #quince_main .quince_txt div:last-child{
-                font-size: 1rem;
-            }
-            #quince_selec_sec .quince-select-title{
-                font-size: 2rem;
-            }
-            .vesti-footer-section-2{
-                border-top: none;
-            }
-            .vesti-footer-section-2 div:first-child{
-                border-right:none;
-            }
-        }
         @media only screen and (min-width: 900px) {
             #main_slider_arrow_cont{
                 display:block;
@@ -238,11 +19,11 @@
 		$(document).ready(function() {
             var slideTimeout = null;
             function setSlider(){
-                // if(!slideTimeout){
-                //     slideTimeout = setInterval(function () {
-                //             $.fn.fullpage.moveSlideRight();
-                //     },5000);
-                // }
+                if(!slideTimeout){
+                    slideTimeout = setInterval(function () {
+                            $.fn.fullpage.moveSlideRight();
+                    },5000);
+                }
             }
             var isReponsive =false;
             function initialization(){
@@ -262,9 +43,6 @@
                             $("#home_main_slider .main_slider_btn").removeClass("col").addClass("col-md-4")
                             $(".quince_thumb").addClass("active");
                         }else{
-                            // if(index == 2){
-                            //     $('#top_middle_sec_row #top_middle_img1,#top_middle_sec_row #top_middle_img2,#top_middle_sec_row #top_middle_img3').addClass('active');
-                            // }
                             $(".vestidos-main-nav-top").removeClass("show")
                         }
 				    },
