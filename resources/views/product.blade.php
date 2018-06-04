2a8f59faf124e0c01bfb2dd0a9a112af59c7f82d
luishk807 <evil_luis@hotmail.com>
2018-06-03 20:44:49 -0400

    working on product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -62,29 +62,83 @@
     cursor: pointer;
 }
 .size_spheres{
-    width: 33px;
+    width: 34px;
     height: 33px;
+    background: transparent;
     display: inline-block;
     border-radius: 18px;
     margin: 2px 2px 2px 0px;
     border: 1px rgba(0,0,0,.2) solid;
     cursor: pointer;
-    font-family:"Arial";
+    font-family: "Arial";
     font-size: .8rem;
+}
+button.size_spheres:hover {
+    background: black;
+    color: white;
 }
 .product_in_btn_pnl{
     margin:25px 0px;
 }
 .product_in_btn{
     background-color: #df1174;
+    border:1px solid #df1174;
     color: white;
-    border: 0px;
-    padding: 10px 100px;
+    padding: 10px 0px;
     font-family: "playfair display";
     text-align:center;
     font-style: italic;
     cursor:pointer;
     font-size:1rem;
+}
+.product_in_btn:hover{
+    background-color: transparent;
+    color: #df1174;
+}
+/***ACCORDEON****/
+.card:first-child{
+    border-bottom: none;
+}
+.card{
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: transparent !important;
+    background-clip: border-box;
+    border-top: 1px solid rgba(0,0,0,.125);
+    border-bottom: 1px solid rgba(0,0,0,.125);
+    border-left:none;
+    border-right:none;
+}
+.card-header{
+    padding: .2rem 1.25rem;
+    margin-bottom: 0;
+    background-color: transparent !important;
+    border-bottom: none;
+}
+.collapse-btn{
+    color:black;
+    text-decoration: none !important;
+    padding:0px;
+}
+#collapseOne{
+    padding: 5px 10px;
+    border-top: 1px solid rgba(0,0,0,.125);
+}
+.product_in_detail_drop{
+    margin: 25px 0px;
+}
+.vestidos-icons-social-black{
+    width: 30px;
+    height: 30px;
+    background-size: 30px 30px;
 }
 </style>
 <div class="main_sub_body main_body_height">
@@ -130,9 +184,39 @@
                                         <button class="size_spheres">10</button>
                                         <button class="size_spheres">13</button>
                                     </div>
-                                    <div class="product_in_detail_drop"></div>
-                                    <div class="product_in_btn_pnl"><button class="product_in_btn">ADD TO CART</button></div>
+                                    <div class="product_in_detail_drop">
+                                        <div id="accordion">
+                                            <div class="card">
+                                                <div class="card-header" id="headingOne">
+                                                    <h5 class="mb-0">
+                                                        <button class="btn btn-link collapse-btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                           + Detail
+                                                        </button>
+                                                    </h5>
+                                                </div>
+                                                <div id="collapseOne" class="collapse" aria-labelleby="headingOne" data-parent="#accordion">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+                                                </div>
+                                            </div>
+                                            <div class="card">
+                                                <div class="card-header" id="headingTwo">
+                                                    <h5 class="mb-0">
+                                                        <button class="btn btn-link collapse-btn" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                                           + Description
+                                                        </button>
+                                                    </h5>
+                                                </div>
+                                                <div id="collapseTwo" class="collapse" aria-labelleby="headingTwo" data-parent="#accordion">
+                                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="product_in_btn_pnl"><button class="btn-block product_in_btn">ADD TO CART</button></div>
                                     <div class="product_in_social">
+                                        
                                     </div>
 
                             </div>
