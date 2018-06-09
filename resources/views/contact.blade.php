995e7eaff539c7693df9ce11ea6a023eef72b9c9
luishk807 <evil_luis@hotmail.com>
2018-06-09 19:07:49 -0400

    add inputs to contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,22 +1,60 @@
 @extends("layouts.sub-layout")
 @section('content')
 <style>
-
+.contact-container{
+    padding-left:0px;
+    padding-right:0px;
+}
 </style>
 <div class="main_sub_body contact_bg main_body_height">
 <div class="container-fluid">
     <div class="row">
 
-        <div class="col-lg-9 container-in-center">
+        <div class="col-md-5 contact-container container-in-center">
             <div>
-               <div class="container-in-space white-md-bg-in contact-col">
-                    <h2>Contact</h2>
+               <div class="container-in-space white-md-bg-in">
                     <div class="container">
                         <div class="row">
-                            <div class="col">
-                            dsfadsf
-                            </div>
-                            <div class="col">
+                            <div class="col-md-7">
+                                <h2>Contact</h2>
+                                <div class="form-group">
+                                        <label for="accountFirstName">First Name:</label>
+                                        <input type="text" id="accountFirstName" class="form-control" name="first_name" value="" placeholder="First Name"/>
+                                        <small class="error">{{$errors->first("first_name")}}</small>
+                                </div>
+                                <div class="form-group">
+                                        <label for="accountLastName">Last Name:</label>
+                                        <input type="text" id="accountLastName" class="form-control" name="last_name" value="" placeholder="Last Name"/>
+                                        <small class="error">{{$errors->first("last_name")}}</small>
+                                </div>
+                                <div class="form-group">
+                                        <label for="accountEmail">Email:</label>
+                                        <input type="email" id="accountEmail" class="form-control" name="email" value="" placeholder="Email"/>
+                                        <small class="error">{{$errors->first("email")}}</small>
+                                </div>
+                                <div class="form-group">
+                                        <label for="accountPhone">Phone:</label>
+                                        <input type="tel" id="accountPhone" class="form-control" name="phone" value="" placeholder="Phone Number"/>
+                                        <small class="error">{{$errors->first("phone")}}</small>
+                                </div>
+                                <div class="form-group">
+                                        <label class="accountCountrySelect" for="accountCountry">Select Country:</label>
+                                        <select class="custom-select accountCountrySelect" name="country" id="accountCountry">
+                                            <option selected>Select Country</option>
+                                        </select>
+                                        <small class="error">{{$errors->first("country")}}</small>
+                                </div>
+                                <div class="form-group">
+                                        <label for="accountQuestion">Question:</label>
+                                        <textarea class="form-control" id="accountQuestion" rows="3" name="question"></textarea>
+                                        <small class="error">{{$errors->first("question")}}</small>
+                                </div>
+                                <div class="vesti_in_btn_pnl">
+                                    <input type="submit" class="btn-block vesti_in_btn" value="Send">
+                                </div>
+
+                            </div><!--end of contact form-->
+                            <div class="col-md-5">
                                 dfasdf
                             </div>
                         </div>
