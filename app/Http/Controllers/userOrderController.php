0e1a5c77d20da24a7a12c78ad0139d4c9b1d1255
Luis Ho <evil_luis@hotmail.com>
2018-07-16 10:29:40 -0400

    add category and brand model to orderController

--- app/Http/Controllers/userOrderController.php
+++ app/Http/Controllers/userOrderController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\vestidosOrders as Orders;
 use App\vestidosStatus as vestidosStatus;
 use App\vestidosUsers as Users;
+use App\vestidosBrands as Brands;
+use App\vestidosCategories as Categories;
 use App\vestidosProducts as Products;
 use Carbon\Carbon as carbon;
 use Illuminate\Support\Facades\Input;
@@ -14,12 +16,14 @@ use App\vestidosUserAddresses as Addresses;
 class userOrderController extends Controller
 {
     //
-    public function __construct(Addresses $addresses, Products $products, Users $users, vestidosStatus $vestidosStatus, Orders $orders){
+    public function __construct(Addresses $addresses, Products $products, Users $users, vestidosStatus $vestidosStatus, Orders $orders,Brands $brands,Categories $categories){
         $this->statuses=$vestidosStatus;
         $this->orders=$orders;
         $this->users=$users;
         $this->products=$products;
         $this->addresses=$addresses;
+        $this->brands=$brands;
+        $this->categories = $categories;
     }
 
     public function index($user_id){
@@ -27,12 +31,16 @@ class userOrderController extends Controller
         $user = $this->users->find($user_id);
         $data["orders"]=$user->orders()->get();
         $data["user"]=$user;
+        $data["brands"]=$this->brands->all();
+        $data["categories"]=$this->categories->all();
         $data["page_title"]="Orders";
         return view("account/orders/home",$data);
     }
     public function viewOrder($order_id,Request $request){
         $data=[];
         $order =$this->orders->find($order_id);
+        $data["brands"]=$this->brands->all();
+        $data["categories"]=$this->categories->all();
         $data["page_title"]="Order: ".$order->order_number;
         $data["order"]=$order;
         $data["user"]=$this->users->find($order->user_id);
@@ -44,6 +52,8 @@ class userOrderController extends Controller
             $order = $this->orders->find($order_id);
             return redirect()->route("user_account",["user_id"=>$order->user_id]);
         }
+        $data["brands"]=$this->brands->all();
+        $data["categories"]=$this->categories->all();
         $data["order"]=$this->orders->find($order_id);
         $data["page_title"]="Delete Orders";
         return view("account/orders/confirm",$data);
