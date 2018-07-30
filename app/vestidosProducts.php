f029c2bd81ecf8b757b3f3c1f6f65bf77024309d
luishk807 <evil_luis@hotmail.com>
2018-07-29 23:36:25 -0400

    add limit to return products to show at bottom on product page

--- app/vestidosProducts.php
+++ app/vestidosProducts.php
@@ -29,7 +29,7 @@ class vestidosProducts extends Model
         DB::raw('(select img_url from vestidos_products_imgs where product_id=vestidos_products.id order by id limit 1) as image_url'),
         DB::raw('(select img_name from vestidos_products_imgs where product_id=vestidos_products.id order by id limit 1) as image_name')
         )
-        ->where("vestidos_products.category_id",$cat_id)->get();
+        ->where("vestidos_products.category_id",$cat_id)->take(5)->get();
         return $products;
     }
     public function searchProductsByLabels($filter){
