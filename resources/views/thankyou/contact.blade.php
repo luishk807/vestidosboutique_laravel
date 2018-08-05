33c5397d1f45f4fe73746846c53a76dc160f1af6
Luis Ho <evil_luis@hotmail.com>
2018-08-05 15:02:42 -0400

    modify initial strucuture for the thank you pages

--- resources/views/thankyou/contact.blade.php
+++ resources/views/thankyou/contact.blade.php
@@ -3,19 +3,25 @@
 <div class="main_sub_body main_body_height">
 <div class="container-fluid">
     <div class="row">
-        <div class="col container-in-center">
+        <div class="col-lg-9 container-in-center">
             <div>
-               <div class="container-in-space white-md-bg-in">
-                        
-
-
-
-                    <h2>Thank you</h2>
-
-               </div><!--end of container-in-space-->
+               <div class="container-in-space missing-col">
+                    <div class="row">
+                        <div class="col">
+                        <img src="{{ asset('images/cloth_error.svg') }}" class="vesti-svg vestidos-icons-tear-dress"/>
+                        </div>
+                        <div class="col">
+                            <h3>404</h3>
+                            <div class="error_msg">Ooops, something goes wrong</div>
+                            <div class="vesti_in_btn_pnl">
+                                <button class="btn-block vesti_in_btn" onclick="location.href='{{ route('home_page')}}'">Return to Home Page</button>
+                            </div>
+                        </div>
+                    </div>
+               </div>
             </div>
-        </div><!--end of container-in-center container-->
-    </div><!--end of row-->
+        </div>
+    </div>
 </div>
 </div>
 @endsection
