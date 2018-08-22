52e15832335ff8cf05daa19d30f995f06cdbd5d7
Luis Ho <evil_luis@hotmail.com>
2018-08-22 11:07:02 -0400

    set default to order reason table

--- database/migrations/2018_08_22_145303_create_order_cancellation_status.php
+++ database/migrations/2018_08_22_145303_create_order_cancellation_status.php
@@ -16,7 +16,7 @@ class CreateOrderCancellationStatus extends Migration
         Schema::create('vestidos_order_reasons', function (Blueprint $table) {
             $table->increments('id');
             $table->integer("name");
-            $table->integer('status')->unsigned()->default(1);
+            $table->integer('status')->default(1)->unsigned();
             $table->foreign("status")->references("id")->on("vestidos_statuses")->onDelete('set null');
             $table->timestamps();
         });
