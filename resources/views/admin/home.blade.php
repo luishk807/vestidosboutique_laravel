bbb0f11970315f77e46a673013990a1ad29459b1
luishk807 <evil_luis@hotmail.com>
2018-09-01 23:43:02 -0400

    working on home admin content

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,31 +3,7 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            @foreach($orders as $order)
-            {{$order->order_number}}
-            @endforeach
-        </div>
-        <div class="col">
-            @foreach($products as $product)
-            {{$product->products_name}}
-            @endforeach
-        </div>
-        <div class="col">
-            @foreach($restocks as $restock)
-            {{$restock->restock_date}}
-            @endforeach
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            @foreach($users as $user)
-                {{$user->first_name}}
-            @endforeach
-        </div>
-        <div class="col">
-            @foreach($rates as $rate)
-                {{$rate->getUser->first_name}}
-            @endforeach
+          
         </div>
     </div>
 </div>
