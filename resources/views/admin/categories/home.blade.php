aaf2e4e521b472fbc51bf8fb8899513284907a2b
luishk807 <luishk807@hotmail.com>
2019-06-23 20:44:47 -0400

    show category and event id

--- resources/views/admin/categories/home.blade.php
+++ resources/views/admin/categories/home.blade.php
@@ -3,16 +3,18 @@
 <div class="container">
     <div class="row container-title">
         <div class="col-md-1"></div>
-        <div class="col-md-3">Name</div>
-        <div class="col-md-1">Status</div>
-        <div class="col-md-3">Action</div>
+        <div class="col-md-1">Id</div>
+        <div class="col-md-4">Name</div>
+        <div class="col-md-2">Status</div>
+        <div class="col-md-4">Action</div>
     </div>
     @foreach($main_items as $category)
     <div class="row container-data row-even">
-    <div class="col-md-1"><input  class="form-control" type="checkbox" name="category_ids[]" value="{{ $category->id }}"></div>
-        <div class="col-md-3">{{$category->name}}</div>
-        <div class="col-md-1">{{ $category->getStatus->name }}</div>
-        <div class="col-md-3 container-button">
+        <div class="col-md-1"><input  class="form-control" type="checkbox" name="category_ids[]" value="{{ $category->id }}"></div>
+        <div class="col-md-1">{{ $category->id }}</div>
+        <div class="col-md-4">{{$category->name}}</div>
+        <div class="col-md-2">{{ $category->getStatus->name }}</div>
+        <div class="col-md-4 container-button">
              <a href="{{ route('confirm_category',['category_id'=>$category->id])}}">delete</a>
             <a href="{{ route('edit_category',['category_id'=>$category->id])}}">edit</a>
         </div>
