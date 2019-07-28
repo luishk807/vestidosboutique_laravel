46dfedb9b5726bd88518670ba6f676155d2bc3b7
Luis Ho <evil_luis@hotmail.com>
2019-07-28 13:52:56 -0400

    avoid error if event has no image

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -79,7 +79,9 @@
                     <div class="col-md-9">
                     --}}
                     <div class="col">
+                        @if($product->img_url)
                         <div class="text-center"><img src="{{ asset('images/shop_banners') }}/{{$event->image_url}}" class="img-fluid" alt/></div>
+                        @endif
                         <div class="shoplist-nav">
                             <ul>
                                 <li>{{ $products->total() }} {{ trans_choice('general.cart_title.product',3) }}</li>
