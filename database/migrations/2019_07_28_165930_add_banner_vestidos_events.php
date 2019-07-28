b5831bc162ad6be00306a76c0e9cf678196672db
Luis Ho <evil_luis@hotmail.com>
2019-07-28 13:08:54 -0400

    add migration

---
+++ database/migrations/2019_07_28_165930_add_banner_vestidos_events.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBannerVestidosEvents extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('vestidos_events', function($table) {
+            $table->text('img_url')->after('name')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('vestidos_events', function($table) {
+            $table->dropColumn('img_url');
+        });
+    }
+}
