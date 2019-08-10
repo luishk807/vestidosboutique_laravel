5d19ac58af23655d447d36b0e51b5a672b1c3811
luishk807 <luishk807@hotmail.com>
2019-08-10 17:02:06 -0400

    update user address migration

--- database/migrations/2018_09_28_024039_create_vestidos_user_address.php
+++ database/migrations/2018_09_28_024039_create_vestidos_user_address.php
@@ -32,7 +32,7 @@ class CreateVestidosUserAddress extends Migration
             $table->foreign("district_id")->references("id")->on("vestidos_districts");
             $table->integer('country_id')->unsigned()->nullable();
             $table->foreign("country_id")->references("id")->on("vestidos_countries");
-            $table->string('zip_code');
+            $table->string('zip_code')->nullable();
             $table->string('phone_number_1');
             $table->string('phone_number_2')->nullable();
             $table->string('email');
