e40517015b6368005f89ebb4d88d7d5855f9ead9
luishk807 <luishk807@hotmail.com>
2019-08-14 00:37:45 -0400

    working on wishlist page

--- resources/views/account/wishlists/home.blade.php
+++ resources/views/account/wishlists/home.blade.php
@@ -16,25 +16,25 @@
     <div class="container account-container">
 
     <div class="row">
-        <div class="col">
+        <div class="col px-0">
 
                 <!--wishlist list begins-->
-                <div class="container account-wishlist-table">
+                <div class="container px-0 account-wishlist-table">
                     @if($wishlists->count() > 0)
-                    <div class="row">
-                        <div class="col image">
+                    <div class="row header">
+                        <div class="col-lg-3 image">
 
                         </div>
-                        <div class="col text-center desc">
+                        <div class="col-lg-7 text-center desc">
                         {{ __('general.user_section.profile_wishlist_desc') }}
                         </div>
-                        <div class="col text-center action">
+                        <div class="col-lg-2 text-center action">
                         {{ __('general.user_section.profile_wishlist_action') }}
                         </div>
                     </div>
                     @foreach($wishlists as $wishlist)
-                    <div class="row">
-                        <div class="col-lg-2 col-md-2 col-sm-12 image">
+                    <div class="row content">
+                        <div class="col-lg-3 col-md-2 col-sm-12 image">
                             <img class="img-fluid" 
                             @if($wishlist->getProduct->images->count()>0)
                             src="{{ asset('images/products')}}/{{$wishlist->getProduct->images->first()->img_url}}" alt="{{$wishlist->getProduct->images->first()->img_name}}"
@@ -46,9 +46,11 @@
                         <div class="col-lg-7 col-md-7 col-sm-12 desc">
                             <strong><a href="{{ route('product_page',['product_id'=>$wishlist->getProduct->id])}}">{{$wishlist->getProduct->products_name}}</a></strong><br/>
                             <div class='rate-view' data-rate-value="{{ $wishlist->getProduct->rates->avg('user_rate') }}"></div>
-                             ${{ number_format($wishlist->getProduct->total_rent,'2','.',',') }}
+                            {{$wishlist->getProduct->product_model}}
+                            <br/>
+                            {{$wishlist->getProduct->product_detail}}
                         </div>
-                        <div class="col-lg-3 col-md-3 col-sm-12 action">
+                        <div class="col-lg-2 col-md-3 col-sm-12 action">
                             <a href='{{ route("deletewishlist",["wishlist_id"=>$wishlist->id])}}'>{{ __('buttons.remove') }}</a>
                         </div>
                     </div>
