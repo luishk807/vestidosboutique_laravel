f4a3c7a2f93e2a82f91a167f74b1ff18063d8ae8
luishk807 <luishk807@hotmail.com>
2020-03-14 16:57:28 -0400

    more testing

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,7 +6,7 @@
  */
 
 require('./bootstrap');
-
+require('./components/App')
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Tasksman</title>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    
+</body><div id="app"></div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
