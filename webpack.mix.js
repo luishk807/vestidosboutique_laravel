24c361212e4614e5b1f691ab6ab0d9984a506914
luishk807 <luishk807@hotmail.com>
2020-03-14 15:52:50 -0400

    adding more react stuff

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,26 @@ let mix = require('laravel-mix');
  */
 
 mix.react('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+.scripts([
+    'resources/assets/js/vestidos.js',
+    'resources/assets/js/vestidos_search.js'
+], 'public/js/vestidos.js')
+.sass('resources/assets/sass/app.scss', 'public/css')
+.sass('resources/assets/sass/app_admin.scss', 'public/css')
+.copy('resources/assets/images/', 'public/images/', false) // Don't flatten!
+.copy('resources/assets/js/vendor/fullpage', 'public/js/vendor/fullpage') // Don't flatten!
+.copy('resources/assets/js/vendor/rater', 'public/js/vendor/rater')
+.copy('resources/assets/js/vestidos_admin.js', 'public/js/vestidos_admin.js')
+.copy('resources/assets/js/vendor/jquery/jquery-3.2.1.min.js', 'public/js/vendor/jquery/jquery-3.2.1.min.js')
+.copy('resources/assets/js/vendor/jquery/popper.min.js', 'public/js/vendor/jquery/popper.min.js')
+.copy('resources/assets/js/vendor/slick', 'public/js/vendor/slick')
+.copy('resources/assets/js/vendor/highchart', 'public/js/vendor/highchart')
+.copy('resources/assets/js/bootstrap.min.js', 'public/js/bootstrap.min.js')
+.copy('app','build/app')
+.copy('public/css','build/css')
+// .copy('public/fonts','build/fonts')
+.copy('public/images','build/images')
+.copy('public/js','build/js')
+.copy('database','build/database')
+.copy('resources','build/resources')
+.copy('routes','build/routes');
